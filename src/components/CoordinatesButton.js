69efee80429f8efe72c7c32bc954d479aaf55722
noelanimanini <noelani26@crystals-MacBook-Pro.local>
2021-02-04 11:14:17 -0800

    Crystal you need to review this lab later. Specifically about the delayed button

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component {
+
+    handleClick = (e) => {
+        let mouseXY = [e.clientX, e.clientY]
+        console.log('I work!')
+        this.props.onReceiveCoordinates(mouseXY)
+        console.log(this.props.test)
+    }
+
+    render () {
+        return(
+            <div>
+                <button onClick={this.handleClick}>button</button>
+            </div>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,31 @@
 // Code DelayedButton Component Here
+import React from 'react' 
+
+export default class DelayedButton extends React.Component {
+    
+    handleClick = (e) => {
+        e.persist();
+        setTimeout(() =>  {
+            this.props.onDelayedClick(e);
+        },
+            this.props.delay)
+    };
+
+
+
+    //     setTimeout(() => {
+    //     this.props.onDelayedClick(e);
+
+    //     this.props.delay
+    //  })
+    // }
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick} >BUTTON</button>
+            </div>
+        )
+    }
+}
